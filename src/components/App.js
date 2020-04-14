1f6affd7a7f4e9aabc4f5628aa360eb99948c140
rsn <rami.serapian@gmail.com>
2020-04-14 17:19:55 -0400

    Begin creating typer component

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./Header";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Typer from "./Typer";
 
 class App extends React.Component {
   render() {
     return (
       <Router>
         <Header></Header>
+        <Route to="/" component={Typer} />
       </Router>
     );
   }
---
+++ src/components/Typer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Typer extends React.Component {
+  render() {
+    return <div>Hello, from typer!</div>;
+  }
+}
+
+export default Typer;
