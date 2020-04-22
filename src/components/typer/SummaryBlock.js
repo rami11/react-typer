7ad1a62cc490f48bc26bffef0f372633d7fc41fb
rsn <rami.serapian@gmail.com>
2020-04-22 15:56:17 -0400

    Make SummaryBlock functional component

--- src/components/typer/SummaryBlock.js
+++ src/components/typer/SummaryBlock.js
@@ -2,21 +2,19 @@ import React from "react";
 import { connect } from "react-redux";
 import { Box } from "@material-ui/core";
 
-class SummaryBlock extends React.Component {
-  render() {
-    return (
-      <Box
-        style={{ margin: "16px", borderRadius: "4px" }}
-        bgcolor="text.primary"
-        color="background.paper"
-        p={2}
-      >
-        Errors: {this.props.errorCount}, Accuracy:{" "}
-        {this.props.accuracyPercentage}%, Speed: {this.props.speed} CPM
-      </Box>
-    );
-  }
-}
+const SummaryBlock = (props) => {
+  return (
+    <Box
+      style={{ margin: "16px", borderRadius: "4px" }}
+      bgcolor="text.primary"
+      color="background.paper"
+      p={2}
+    >
+      Errors: {props.errorCount}, Accuracy: {props.accuracyPercentage}%, Speed:{" "}
+      {props.speed} CPM
+    </Box>
+  );
+};
 
 const mapStateToProps = (state) => {
   return {
