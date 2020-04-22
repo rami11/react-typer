91d18f1bed3963caf4f9a395294c6cec9d0deae1
rsn <rami.serapian@gmail.com>
2020-04-22 14:22:08 -0400

    Add theme provide to App component

--- src/components/App.js
+++ src/components/App.js
@@ -3,18 +3,23 @@ import Header from "./Header";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Typer from "../components/typer/Typer";
 import { Container } from "@material-ui/core";
+import { ThemeProvider } from "react-jss";
 
-class App extends React.Component {
-  render() {
-    return (
+const theme = {
+  colorPrimary: "blue",
+};
+
+const App = () => {
+  return (
+    <ThemeProvider theme={theme}>
       <Router>
         <Header></Header>
         <Container maxWidth="sm">
           <Route to="/" component={Typer} />
         </Container>
       </Router>
-    );
-  }
-}
+    </ThemeProvider>
+  );
+};
 
 export default App;
